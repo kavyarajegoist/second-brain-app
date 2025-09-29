1295581cf21d5c0e40f545f834448cb5f055a057
kavyarajegoist <kavyarajmangrola16@gmail.com>
2025-09-29 13:40:41 +0000

    fix:addContent route to first add the tags and then add the content to the database

--- backend/src/routes/content.ts
+++ backend/src/routes/content.ts
@@ -1,37 +1,32 @@
-import  { type Request, type Response,Router } from "express";
-import {type AuthRequest } from "../middleware/user";
+import { type Request, type Response, Router } from "express";
+import { type AuthRequest } from "../middleware/user";
 import userAuth from "../middleware/user";
-import { Content } from "../db";
+import { Content, Tag } from "../db";
 import { contentSchema } from "../schema/content";
+import type mongoose from "mongoose";
 
 const contentRouter = Router();
 
+contentRouter.get("/", userAuth, async (req: AuthRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    const contents = await Content.find({ userId }).populate({
+      path: "userId",
+      model: "user", 
+      select: "username -_id",
+    });
 
-contentRouter.get(
-  "/",
-  userAuth,
-  async (req: AuthRequest, res: Response) => {
-    try {
-      const userId = req.user?.id;
-      const contents = await Content.find({ userId })
-      .populate({
-        path: "userId",
-        model: "user", // Make sure this matches your model name in mongoose
-        select: "username -_id"
-      });
-
-      if (!contents) {
-        res.status(403).json({ message: "No Content found" });
-      }
-      res.status(200).json({ message: "content founde", contents });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Internal Server Error" });
+    if (!contents) {
+      res.status(403).json({ message: "No Content found" });
     }
+    res.status(200).json({ message: "content founde", contents });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-);
+});
 
-contentRouter.post("/add-content", userAuth, async (req: AuthRequest, res) => {
+contentRouter.post("/add", userAuth, async (req: AuthRequest, res) => {
   try {
     const userId = req.user?.id;
     const parsedData = contentSchema.safeParse(req.body);
@@ -41,55 +36,82 @@ contentRouter.post("/add-content", userAuth, async (req: AuthRequest, res) => {
 
       res.status(403).json({
         message: "Invalid Input",
-        errorMessages:errorMessages.flatten(),
+        errorMessages: errorMessages.flatten(),
       });
+      return;
+    }
+    const tagIds: mongoose.Types.ObjectId[] = [];
+    const { link, type, title, tags: tagTitle } = parsedData.data;
+    const content = await Content.find({title,userId});
+    if (content)
+    {
+      res.status(403).json({message:"Content already created with given title. Title should be unique"});
+      return;
     }
 
-    console.log("hello");
-    await Content.create({
-      type: parsedData.data?.type,
-      link: parsedData.data?.link,
+    // console.log(tagTitle)
+    for (const t of tagTitle) {
+      // console.log(t)
+      const doc = await Tag.findOneAndUpdate(
+        { title: t.trim().toLowerCase() },
+        {
+          $setOnInsert: { title:t.trim().toLowerCase() },
+        },
+        {
+          upsert:true,new:true
+        }
+      );
+      if (doc?._id) tagIds.push(doc._id)
+    }
 
-      title: parsedData.data?.title,
-      tags: parsedData.data?.tags,
+    // console.log("hello");
+    await Content.create({
+      link,
+      type,
+      title,
       userId,
+      tags:tagIds
     });
 
     res.status(201).json({ message: "Contend add successfully" });
-    console.log("hellp");
+    // console.log("hellp");
   } catch (err) {
     console.log(err);
-    res.status(401).json({
+    res.status(500).json({
       message: "error occured",
       err,
     });
   }
 });
 
-contentRouter.delete("/content/:id", userAuth, async (req: AuthRequest, res) => {
-  try {
-    const userId = req.user?.id;
-    const contentId = req.params?.id;
-    console.log(contentId);
-    if (!contentId) {
-      res.status(404).json({ message: "Content id is required" });
-      return;
-    }
-    if (!userId) {
-      res.status(401).json({ message: "user not authenticated" });
-      return;
-    }
+contentRouter.delete(
+  "/content/:id",
+  userAuth,
+  async (req: AuthRequest, res) => {
+    try {
+      const userId = req.user?.id;
+      const contentId = req.params?.id;
+      console.log(contentId);
+      if (!contentId) {
+        res.status(404).json({ message: "Content id is required" });
+        return;
+      }
+      if (!userId) {
+        res.status(401).json({ message: "user not authenticated" });
+        return;
+      }
 
-    const result = await Content.findOneAndDelete({ userId, _id: contentId });
-    if (!result) {
-      res.status(403).json({ message: "Content not found" });
-      return;
+      const result = await Content.findOneAndDelete({ userId, _id: contentId });
+      if (!result) {
+        res.status(403).json({ message: "Content not found" });
+        return;
+      }
+      res.status(200).json({ message: "Content deleted successfully." });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-    res.status(200).json({ message: "Content deleted successfully." });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
   }
-});
+);
 
 export default contentRouter;
--- backend/src/schema/content.ts
+++ backend/src/schema/content.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const contentSchema = z.object({
     title: z.string().min(4,"Title should of atleat four character").max(30,"Title should not me more than then characters"),
-    type: z.string().min(3,"Type should minimum of 3 letter"),
+    type: z.enum(['document','tweet','links','video']),
     tags: z.array(z.string()),
     link: z.string().url("Given link is not an url."),
 
