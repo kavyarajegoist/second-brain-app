9cd8e9e379d4d03348557e3a6e2c510ba7657339
kavyarajegoist <kavyarajmangrola16@gmail.com>
2025-10-01 09:53:16 +0000

    fix:the share route to check the lenght of the content array

--- backend/src/routes/brain.ts
+++ backend/src/routes/brain.ts
@@ -1,12 +1,10 @@
-import { type Request, type Response,Router } from "express";
-import {type AuthRequest } from "../middleware/user";
+import { type Request, type Response, Router } from "express";
+import { type AuthRequest } from "../middleware/user";
 import userAuth from "../middleware/user";
-import { Content,Links } from "../db";
+import { Content, Links } from "../db";
 import CryptoJS from "crypto-js";
 
-
 const brainRouter = Router();
-
 
 brainRouter.post(
   "/share",
@@ -15,39 +13,36 @@ brainRouter.post(
     const userId = req.user?.id;
     const share: boolean = req.body.share;
     try {
-
       if (!userId) {
         res.status(401).json({ message: "User not authorised" });
         return;
       }
-    
 
       if (share) {
         const hash = CryptoJS.SHA224(userId + Date.now())
           .toString(CryptoJS.enc.Hex)
           .slice(0, 16);
-       
-
-        await Links.create({
-          hash: hash,
-          userId,
-        });
-        
-        
-          res.status(200)
-          .json({
-            shareLink: `/${hash}`,
+        console.log(hash);
+        const link = await Links.findOne({ hash, userId });
+        console.log(link);
+        if (link) {
+          res.status(201).json({ shareLink: `/${link?.hash}` });
+        } else {
+          await Links.create({
+            hash: hash,
+            userId,
           });
-        
+          res.status(200).json({
+            shareLink: `${process.env.FRONTEND_URL!}/brain?shareLink=${hash}`,
+          });
+        }
       }
     } catch (error) {
       console.log("Main error" + error);
     }
-    if(!share)
-    {
-      await Links.deleteOne({userId});
+    if (!share) {
+      await Links.deleteOne({ userId });
     }
-
   }
 );
 
@@ -55,7 +50,7 @@ brainRouter.get("/:sharehash", async (req: Request, res: Response) => {
   try {
     const hash = req.params?.sharehash;
     console.log(hash);
-    
+
     if (!hash) {
       res.status(403).json({
         message: "Invalid Link",
@@ -63,12 +58,21 @@ brainRouter.get("/:sharehash", async (req: Request, res: Response) => {
       return;
     }
 
-    const link = await Links.findOne({hash});
-    console.log(link)
+    const link = await Links.findOne({ hash });
+    console.log(link);
     const userId = link?.userId;
-    const content = await Content.find({userId: userId}).select('title tags -userId  link ').populate({path:"userId",model:'user',select:"username -_id"});
-    if (!content) {
-      res.status(404).json({ message: "Content with give id is not found." });
+    const content = await Content.find({ userId: userId })
+      .select("title tags type createdAt link _id")
+      .populate({ path: "userId", model: "user", select: "username -_id" });
+    if (!userId) {
+      res.status(404).json({ message: "Share link not found." });
+      return;
+    }
+
+    if (!content.length) {
+      res
+        .status(404)
+        .json({ message: "No content found for this share link." });
       return;
     }
 
